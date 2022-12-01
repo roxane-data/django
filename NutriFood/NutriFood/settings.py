f1555ecbd1b1dee0525bd43a55eee607a8ac6dd9
roxane-data <ajavonroxane@gmail.com>
2022-12-01 12:42:00 +0100

    updating settings.py postgresql

--- NutriFood/NutriFood/settings.py
+++ NutriFood/NutriFood/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'NutriFood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'open_food_facts',
+        'USER': 'roxane',
+        'PASSWORD': 'noamyann-139',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
